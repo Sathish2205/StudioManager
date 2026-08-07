53e819ca7d41ed34dcbb1e8dfa3afac77f87e884
Sathish <sathishsaravanan2205@gmail.com>
2026-08-07 22:24:02 +0530

    Refactor components and pages into individual dedicated folders

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
-import Sidebar from '../components/Sidebar'
-import DashboardHeader from '../components/DashboardHeader'
+import Sidebar from '../../components/Sidebar'
+import DashboardHeader from '../../components/DashboardHeader'
 import './Dashboard.css'
 
 export default function Dashboard() {

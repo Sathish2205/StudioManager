c1ed37aa010a5a8a775953d9250cedacfd4ac0fb
Sathish <sathishsaravanan2205@gmail.com>
2026-09-09 22:07:09 +0530

    fix: import missing AppLayout in App.jsx to resolve blank screen on page navigation

--- src/App.jsx
+++ src/App.jsx
@@ -25,6 +25,7 @@ import AddEmployeePage from './pages/AddEmployeePage/AddEmployeePage'
 
 import { AuthProvider, useAuth } from './context/AuthContext'
 import Login from './pages/Login/Login'
+import AppLayout from './components/AppLayout'
 import AccessDenied from './components/AccessDenied/AccessDenied'
 import ErrorBoundary from './components/ErrorBoundary'
 import { ROUTE_PERMISSIONS } from './constants/roles'
